d5f9c650b221134f667087e40d6b049131cdc641
Danica Valdez <danicavaldez@Danicas-MacBook-Pro.local>
2019-05-25 11:30:07 -0700

    Callbacks completed

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -27,25 +27,44 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+getLength(items, itemsLength => console.log(itemsLength));
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+last(items, lastItem => console.log(lastItem));
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+sumNums(21, 40, add => console.log(add));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+multiplyNums(17, 32, product => console.log(product));
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let hasItem = false;
+
+  list.forEach((listedItem) => {
+    if (item === listedItem) {
+      hasItem = true;
+    }
+  });
+  
+  return cb(hasItem);
 }
 
+contains("Pencil", items, checkItems => console.log(checkItems));
+contains("Banana", items, checkItems => console.log(checkItems));
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
